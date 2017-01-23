ca24c4971cdefbaba0a84b87b7c50043e5cd63b3
noam finkelstein <noam.finkelstein@gmail.com>
2017-01-22 21:07:49 -0500

    Complete nucleotide count

--- elixir/nucleotide-count/nucleotide_count.exs
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -13,10 +13,22 @@ defmodule NucleotideCount do
   1
   """
   @spec count([char], char) :: non_neg_integer
-  def count(strand, nucleotide) do
-
+  def count(strand, nucleotide) do 
+    count(strand, nucleotide, 0) 
   end
 
+  @spec count([char], char, integer) :: non_neg_integer
+  def count(strand, nucleotide, num) do 
+    if length(strand) > 0 do
+    count(tl(strand), nucleotide, if hd(strand) == nucleotide do
+                                    num + 1 
+                                  else
+                                    num
+                                  end)
+    else
+      num
+    end
+  end
 
   @doc """
   Returns a summary of counts by nucleotide.
@@ -28,6 +40,7 @@ defmodule NucleotideCount do
   """
   @spec histogram([char]) :: map
   def histogram(strand) do
-
+    for nucleotide <- @nucleotides, into: %{},
+      do: {nucleotide, count(strand, nucleotide)}
   end
 end
