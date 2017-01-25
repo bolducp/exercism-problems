4da015f25b414007f0180d8b7b00078ff8f8ba02
noam <noam.finkelstein@gmail.com>
2017-01-24 20:10:37 -0500

    Complete Bob problems

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,14 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+      String.length(String.trim(input)) == 0 ->
+        "Fine. Be that way!"
+      String.ends_with?(input, "?") ->
+        "Sure."
+      String.upcase(input) == input and String.downcase(input) != input ->
+        "Whoa, chill out!"
+      true ->
+        "Whatever."
     end
   end
 end
