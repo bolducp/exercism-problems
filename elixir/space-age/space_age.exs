3d2a144ba6c67f2b023006d87200f59986f256d8
noam <noam.finkelstein@gmail.com>
2017-01-24 19:55:28 -0500

    Solve space age

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -8,6 +8,17 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-
+    earth_year = seconds / 31557600
+    relative_times = [
+      earth: 1.0,
+      mercury: 0.2408467,
+      venus: 0.61519726,
+      mars: 1.8808158,
+      jupiter: 11.862615,
+      saturn: 29.447498,
+      uranus: 84.016846,
+      neptune: 164.79132,
+    ]
+    earth_year / relative_times[planet]
   end
 end
